13d86253104bee36758d3c80b8b197b0ba60a300
Aidan Edwards <Aidan.S.Edwards@gmail.com>
2020-05-13 11:24:04 -0500

    Reformat code

--- index.js
+++ index.js
@@ -5,28 +5,28 @@ const fs = require('fs');
 const commandExists = require('command-exists');
 
 const managers = {
-  "apt-get": {
-      "check": "apt-get",
-      "command": "sudo apt-get update && sudo apt-get install"
+    "apt-get": {
+        "check": "apt-get",
+        "command": "sudo apt-get update && sudo apt-get install"
     },
-  "brew": {
-      "check": "brew",
-      "command": "brew update && brew install"
+    "brew": {
+        "check": "brew",
+        "command": "brew update && brew install"
     }
 };
 
 try {
 
-  var pkgs;
+    var pkgs;
 
-  for (let [mgr, info] of Object.entries(managers)) {
-    if (commandExists.sync(info.check)) {
-      pkgs = core.getInput(mgr);
-      if (pkgs) {
-        child_process.execSync(info.command + " " + pkgs)
-      }
+    for (let [mgr, info] of Object.entries(managers)) {
+        if (commandExists.sync(info.check)) {
+            pkgs = core.getInput(mgr);
+            if (pkgs) {
+                child_process.execSync(info.command + " " + pkgs)
+            }
+        }
     }
-  }
 
 } catch (error) {
     core.setFailed(error.message);
